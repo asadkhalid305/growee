ff277c7a07dfa55c94e21663a725ddc892a9f205
asadkhalid305 <asadkhalid305@gmail.com>
2019-08-20 00:48:39 +0500

    removed redundant code

--- js/login.js
+++ js/login.js
@@ -1,14 +1,5 @@
 $(document).ready(function () {
     $("#login-btn").click(function () {
-        // $.get("http://localhost:3010/auth",
-        //     {
-        //         user: "admin",
-        //         password: "growee"
-        //     },
-        //     function (data, status) {
-        //         alert("Data: " + data + "\nStatus: " + status);
-        //     });
-
         $.ajax({
             type: 'POST',
             url: "http://localhost:3010/auth",
